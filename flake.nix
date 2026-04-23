aa69af49d89ac3b058366a57f73c0c3b6e87f118
Aleix Roca Nonell <aleix.rocanonell@bsc.es>
2026-04-23 08:14:35 +0200

    Add overlay

--- flake.nix
+++ flake.nix
@@ -9,18 +9,20 @@
   let
     lib = nixpkgs.lib;
     pkgs = import nixpkgs { system = "x86_64-linux"; };
-  in {
-    legacyPackages.x86_64-linux = with pkgs; {
-      aff = callPackage pkgs/aff {};
-      dthp = callPackage pkgs/dthp/default.nix {};
-      lavg  = callPackage pkgs/lavg {};
-      libnest = callPackage pkgs/libnest {};
-      mbm = callPackage pkgs/mbm/default.nix {};
-      nex = callPackage pkgs/nex/default.nix {};
-      pthtree = callPackage pkgs/pthtree {};
-      setaff = callPackage pkgs/setaff {};
-      wicf = callPackage pkgs/bpf/wicf {};
-      inherit perf bpftrace;
+    mkPackages = p: {
+      aff = p.callPackage pkgs/aff {};
+      dthp = p.callPackage pkgs/dthp/default.nix {};
+      lavg  = p.callPackage pkgs/lavg {};
+      libnest = p.callPackage pkgs/libnest {};
+      mbm = p.callPackage pkgs/mbm/default.nix {};
+      nex = p.callPackage pkgs/nex/default.nix {};
+      pthtree = p.callPackage pkgs/pthtree {};
+      setaff = p.callPackage pkgs/setaff {};
+      wicf = p.callPackage pkgs/bpf/wicf {};
+      inherit (p) perf bpftrace;
     };
+  in {
+    legacyPackages.x86_64-linux = mkPackages pkgs;
+    overlays.default = final: prev: mkPackages final;
   };
 }
